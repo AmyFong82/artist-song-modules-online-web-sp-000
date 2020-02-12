3e5b5116aee02b6aa04c30b9b600cf5da4d53dfd
Amy Fong <amyhfong@gmail.com>
2020-02-12 17:22:42 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-  
+
   module ClassMethods
 
     def reset_all
@@ -8,6 +8,12 @@ module Memorable
 
     def count
       all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
     end
   end
 
